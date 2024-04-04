f240f66893735e340bd454b3ed0b5e391919c42a
123harri <harrietwanjiku90@gmail.com>
2024-04-04 13:13:31 -0700

    a

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,27 @@
 #!/usr/bin/env bash
-
-# Update package repositories
+# script that sets up web servers for the deployment of web_static
 sudo apt-get update
-
-# Install Nginx
 sudo apt-get -y install nginx
-
-# Allow Nginx through firewall
 sudo ufw allow 'Nginx HTTP'
 
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
-
-# Create a fake HTML file
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
-<head>
-</head>
-<body>
-  Holberton School
-</body>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create symbolic link
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# Set ownership
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-# Restart Nginx
 sudo service nginx restart
