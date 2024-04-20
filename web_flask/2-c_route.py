e7ad916acc33471f111a586d853cb8fcf3747a78
123harri <harrietwanjiku90@gmail.com>
2024-04-20 08:22:38 +0000

    a

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application.
+
+The web application listens on 0.0.0.0, port 5000, and includes three routes:
+- `/`: Displays "Hello HBNB!"
+- `/hbnb`: Displays "HBNB"
+- `/c/<text>`: Displays "C " followed by the value of the text variable,
+  replacing underscore _ symbols with a space.
+
+The option strict_slashes=False is used in the route definitions.
+"""
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display "Hello HBNB!" when accessing the root URL."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Display "HBNB" when accessing the /hbnb URL."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """Display "C " followed by the value of the text variable."""
+    return 'C {}'.format(escape(text).replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application.
+
+The web application listens on 0.0.0.0, port 5000, and includes six routes:
+- `/`: Displays "Hello HBNB!"
+- `/hbnb`: Displays "HBNB"
+- `/c/<text>`: Displays "C " followed by the value of the text variable,
+  replacing underscore _ symbols with a space.
+- `/python/<text>`: Displays "Python " followed by the value of the text variable,
+  replacing underscore _ symbols with a space. The default value of text is "is cool".
+- `/number/<n>`: Displays "n is a number" only if n is an integer.
+- `/number_template/<n>`: Displays a HTML page only if n is an integer.
+  The HTML page contains an H1 tag with the text "Number: n".
+
+The option strict_slashes=False is used in the route definitions.
+"""
+
+from flask import Flask, escape, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display "Hello HBNB!" when accessing the root URL."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Display "HBNB" when accessing the /hbnb URL."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """Display "C " followed by the value of the text variable."""
+    return 'C {}'.format(escape(text).replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_text(text):
+    """Display "Python " followed by the value of the text variable."""
+    return 'Python {}'.format(escape(text).replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """Display "n is a number" only if n is an integer."""
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_number_template(n):
+    """Display an HTML page with an H1 tag containing the text "Number: n"."""
+    if isinstance(n, int):
+        return render_template('number_template.html', number=n)
+    else:
+        return 'Not found', 404
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
